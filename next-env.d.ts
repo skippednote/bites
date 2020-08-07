ab139da100131d977e51b15c4eae0b98f0669b0b
Bassam Ismail <skippednote@gmail.com>
2020-08-07 06:39:30 +0530

    revert the custom type definition

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-
-export declare namespace next-auth {
-  client {
-    var useSession: any;
-    var getSession: any;
-  }
-}
